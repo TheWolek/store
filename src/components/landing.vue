ac39d0fb6096222040224f9ca509f2dd2812bcfb
TheWolek <pawelm916@wp.pl>
2021-05-07 22:13:08 +0200

    hellowordl > landing

---
+++ src/components/landing.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="col-12"></div>
+</template>
+
+<script>
+export default {
+  name: "landing",
+};
+</script>
+
+<style scoped lang="scss"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <landing msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import landing from "@/components/landing.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    landing,
   },
 };
 </script>
