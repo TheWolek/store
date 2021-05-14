25cd2e761cb3c466c3642667e69e67e7cec3e825
TheWolek <pawelm916@wp.pl>
2021-05-14 23:14:53 +0200

    del styles from landing.vue

--- src/components/landing.vue
+++ src/components/landing.vue
@@ -13,20 +13,3 @@ export default {
   name: "landing",
 };
 </script>
-
-<style scoped lang="scss">
-@import "../styles/common.scss";
-
-.landing {
-}
-
-@media screen and (min-width: 768px) {
-  .landing {
-  }
-}
-
-@media screen and (min-width: 1024px) {
-  .landing {
-  }
-}
-</style>
