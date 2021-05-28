fe73ae5eb24bc58fc2aa76f60fe080fa74654b56
TheWolek <pawelm916@wp.pl>
2021-05-28 19:05:26 +0200

    productpage fast build

--- src/components/productList/productPage.vue
+++ src/components/productList/productPage.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>{{ item }}</div>
+  <div class="productPage">
+    <div class="productImgHolder">
+      <img src="https://via.placeholder.com/450x650" />
+    </div>
+    <div class="productDetails">
+      <h1>{{ item.name }}</h1>
+      <p class="newPrice">{{ item.newPrice }}</p>
+      <p class="regPrice">{{ item.regularPrice }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -16,4 +25,32 @@ export default {
 
 <style lang="scss" scoped>
 @import "../../styles/common.scss";
+
+.productPage {
+  display: flex;
+  justify-content: space-around;
+
+  .productImgHolder {
+    overflow: hidden;
+  }
+
+  .productDetails {
+    display: flex;
+    flex-wrap: wrap;
+
+    h1 {
+      width: 100%;
+    }
+
+    .regPrice {
+      text-decoration: line-through;
+    }
+
+    .newPrice {
+      color: $font-red;
+      font-weight: bold;
+      font-size: 1.2rem;
+    }
+  }
+}
 </style>
