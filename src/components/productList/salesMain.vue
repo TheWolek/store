34ef5d100590f299969f55424bd0592e69687d10
TheWolek <pawelm916@wp.pl>
2021-06-05 18:09:05 +0200

    fetching collection in sales

--- src/components/productList/salesMain.vue
+++ src/components/productList/salesMain.vue
@@ -28,26 +28,37 @@ export default {
   data: function () {
     return {
       //depedning of what gender is selected, diffrent collection would be fetch from DB
-      collection: [
-        { id: 0, name: "T-shirt", regularPrice: 39.99, newPrice: 29.99 },
-        { id: 1, name: "shirt", regularPrice: 89.99, newPrice: 59.99 },
-        { id: 2, name: "jacket", regularPrice: 69.99, newPrice: 49.99 },
-        { id: 3, name: "T-shirt", regularPrice: 119.99, newPrice: 100 },
-        { id: 4, name: "shirt", regularPrice: 40, newPrice: 30.99 },
-        { id: 5, name: "shirt", regularPrice: 75, newPrice: 65 },
-        { id: 6, name: "jacket", regularPrice: 129.99, newPrice: 99.99 },
-        { id: 7, name: "T-shirt", regularPrice: 29.99, newPrice: 19 },
-      ],
+      // collection: [
+      //   { id: 0, name: "T-shirt", regularPrice: 39.99, newPrice: 29.99 },
+      //   { id: 1, name: "shirt", regularPrice: 89.99, newPrice: 59.99 },
+      //   { id: 2, name: "jacket", regularPrice: 69.99, newPrice: 49.99 },
+      //   { id: 3, name: "T-shirt", regularPrice: 119.99, newPrice: 100 },
+      //   { id: 4, name: "shirt", regularPrice: 40, newPrice: 30.99 },
+      //   { id: 5, name: "shirt", regularPrice: 75, newPrice: 65 },
+      //   { id: 6, name: "jacket", regularPrice: 129.99, newPrice: 99.99 },
+      //   { id: 7, name: "T-shirt", regularPrice: 29.99, newPrice: 19 },
+      // ],
+      collection: [],
       loading: true,
     };
   },
   mounted: function () {
+    this.fetchData(this.gender);
     window.setTimeout(() => {
       this.loading = false;
     }, 500);
     window.setTimeout(() => {
       $(".itemsWrap").css("opacity", "1");
     }, 500);
+  },
+  methods: {
+    fetchData: function (gender) {
+      if (gender == 0) gender = "female";
+      else gender = "male";
+      const data = fetch("http://localhost:5000/sales/" + gender)
+        .then((res) => res.json())
+        .then((data) => (this.collection = data));
+    },
   },
 };
 </script>
